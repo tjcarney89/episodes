b468552aed659ce6c579650718a2767a7cf709ca
TJ Carney <tjcarney@TJs-MacBook-Air.local>
2017-03-20 13:37:31 -0400

    Initial Commit

---
+++ Episodes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Episodes
+//
+//  Created by TJ Carney on 3/20/17.
+//  Copyright © 2017 TJ Carney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
